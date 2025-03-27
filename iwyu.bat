f7d9567fa20a04f97b90ea7b54bbcbaa017f20a3
ThosRTanner <ThosRTanner@users.noreply.github.com>
2025-03-27 21:43:59 +0000

    Do as much as possible to automate n++ release (#31)

--- iwyu.bat
+++ iwyu.bat
@@ -1,5 +1,5 @@
 SET IWYU_PATH="..\Include What You Use\include-what-you-use"
 SET IWYU=%IWYU_PATH%\iwyu_tool.py
 if exist %IWYU% (
-	python.exe %IWYU% -j 5 -v -p .\compile_commands.json -o clang-warning -- -Xiwyu --mapping_file=vs2022cpp20.imp --include=stdafx.h
+	python.exe %IWYU% -j 5 -v -p .\compile_commands.json -o clang-warning -- -Xiwyu --mapping_file=vs2022cpp20.imp --include=stdafx.h -D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH
 )
